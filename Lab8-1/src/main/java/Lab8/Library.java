79c3a0ba337e13be60bf05e2a8c0b821b712f22d
sharpsonmong <monghoyam@gmail.com>
2019-11-05 18:00:20 +0800

    commit

--- Lab8-1/src/main/java/Lab8/Library.java
+++ Lab8-1/src/main/java/Lab8/Library.java
@@ -11,7 +11,7 @@ public class Library {
 	public static void main(String arg[]) {
 		Library lib = new Library();
 		if(lib.someLibraryMethod()) {
-			System.out.println("When there is a fire, commit and push.");
+			System.out.println("When there is  a fire, commit and push.");
 		}
 	}
 	
