55212aca73c907c65f5f31ed01e303c0eb5286a4
sharpsonmong <monghoyam@gmail.com>
2019-11-05 17:27:16 +0800

    changed

--- Lab8-1/src/main/java/Lab8/Library.java
+++ Lab8-1/src/main/java/Lab8/Library.java
@@ -12,6 +12,7 @@ public class Library {
 		Library lib = new Library();
 		if(lib.someLibraryMethod()) {
 			System.out.println("When there is a fire, commit and push.");	
+			System.out.println("When there is a fire, commit and push.");	
 		}
 	}
 	
